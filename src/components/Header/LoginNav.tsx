0ea17851bce9e31957db32898dc439723cc2c111
hseun <hseun0732@dms.hs.kr>
2025-07-05 15:37:58 +0900

    chore :: data

--- src/components/Header/LoginNav.tsx
+++ src/components/Header/LoginNav.tsx
@@ -1,14 +1,14 @@
-import styled from "@emotion/styled"
-import RoundButton from "../Common/Button/RoundButton"
-import { Color, Font } from "../../styles"
-import { useNavigate } from "react-router-dom"
+import styled from "@emotion/styled";
+import RoundButton from "../Common/Button/RoundButton";
+import { Color, Font } from "../../styles";
+import { useNavigate } from "react-router-dom";
 // import { useGetUserInfo } from "../../hooks/query/auth"
 
 const LoginNav = () => {
   const navigate = useNavigate();
 
   const isLoggedIn = true; // false로 바꾸면 '로그인' 버튼이 보입니다.
-  const dummyUserInfo = { name: "임다영" };
+  const dummyUserInfo = { name: "관리자" };
 
   // const { data: userInfo, isLoading, isError } = useGetUserInfo();
   // const isLoggedIn = !!userInfo;
@@ -20,19 +20,16 @@ const LoginNav = () => {
           {dummyUserInfo.name}님
         </UserName>
       ) : (
-        <RoundButton
-          text="로그인"
-          onClick={() => navigate("/login")}
-        />
+        <RoundButton text="로그인" onClick={() => navigate("/login")} />
       )}
     </NavWrapper>
-  )
-}
+  );
+};
 
 const NavWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-`
+`;
 
 const UserName = styled.div`
   max-width: 100px;
@@ -44,6 +41,6 @@ const UserName = styled.div`
   &:hover {
     color: ${Color.gray700};
   }
-`
+`;
 
 export default LoginNav;
